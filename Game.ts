2b6699ef19f98dc47f303f7df89a72a23af2024c
qz23wxe4crvtbnpmkl <168938678+qz23wxe4crvtbnpmkl@users.noreply.github.com>
2024-06-30 17:20:42 -0400

    Added Monkey WebSocket Interception Method.

--- Game.ts
+++ Game.ts
@@ -1,28 +1,51 @@
 const msgpack = require("msgpack");
 
 class Msgpack {
-    public encode(data: any) {
+    public encode(data: any): Buffer {
         return msgpack.encode(data);
     }
 
-    public decode(data: any) {
+    public decode(data: Buffer): any {
         return msgpack.decode(data);
     }
 }
 
 class WS extends Msgpack {
-    private ws: any;
+    private ws: WebSocket | null;
 
-    public Send(type: any) {
-        var data = Array.prototype.slice.call(arguments, 1);
-        this.ws.send(super.encode([type, data]));
+    constructor() {
+        super();
+        this.ws = null;
     }
 
-    public HandlePackets(data: any) {
+    public send(type: string,...data: any[]): void {
+        if (!this.ws) {
+            throw new Error("WebSocket is not initialized");
+        }
+        this.ws.send(this.encode([type,...data]));
+    }
 
+    public handlePackets(data: any): void {
+        data = new Uint8Array(data);
+        const parsed = this.decode(data);
+        const type: string = parsed[0];
+        const packetData = parsed[1];
+
+        if (type === "a") {
+            this.send("6", "ITS WORKING");
+        }
     }
 }
 
-class Game extends WS {
+WebSocket.prototype.send = function(packet: any): void {
+    const ws = this;
+    if (!(ws as any).ws) {
+        (ws as any).ws = new WS();
+        ws.addEventListener("message", (msg) => {
+            (ws as any).ws.handlePackets(msg.data);
+        });
+    }
+    (ws as any).ws.send(packet);
+};
 
-}
+class Game extends WS {}
